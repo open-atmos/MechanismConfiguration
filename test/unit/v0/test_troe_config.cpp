0d6059603761ba39bf13f98de68050620382831f
Kyle Shores <kyle.shores44@gmail.com>
2025-02-04 14:27:46 -0600

    v0 troe

--- test/unit/v0/test_troe_config.cpp
+++ test/unit/v0/test_troe_config.cpp
@@ -1,130 +1,155 @@
-#include <micm/configure/solver_config.hpp>
-
 #include <gtest/gtest.h>
+
+#include <mechanism_configuration/v0/parser.hpp>
+#include <mechanism_configuration/constants.hpp>
+#include <mechanism_configuration/conversions.hpp>
+
+using namespace mechanism_configuration;
 
 TEST(TroeConfig, DetectsInvalidConfig)
 {
-  micm::SolverConfig solver_config;
+  v0::Parser parser;
+  std::vector<std::string> extensions = { ".json", ".yaml" };
+  for (auto& extension : extensions)
+  {
+    std::string file = "./v0_unit_configs/troe/missing_reactants/config" + extension;
+    auto parsed = parser.Parse(file);
+    EXPECT_FALSE(parsed);
+    EXPECT_EQ(parsed.errors.size(), 1);
+    EXPECT_EQ(parsed.errors[0].first, ConfigParseStatus::RequiredKeyNotFound);
+    for (auto& error : parsed.errors)
+    {
+      std::cout << error.second << " " << configParseStatusToString(error.first) << std::endl;
+    }
 
-  // Read and parse the configure files
-  try
-  {
-    solver_config.ReadAndParse("./v0_unit_configs/troe/missing_reactants");
-  }
-  catch (const std::system_error& e)
-  {
-    EXPECT_EQ(e.code().value(), static_cast<int>(MicmConfigErrc::RequiredKeyNotFound));
-  }
-  try
-  {
-    solver_config.ReadAndParse("./v0_unit_configs/troe/missing_products");
-  }
-  catch (const std::system_error& e)
-  {
-    EXPECT_EQ(e.code().value(), static_cast<int>(MicmConfigErrc::RequiredKeyNotFound));
+    file = "./v0_unit_configs/troe/missing_products/config" + extension;
+    parsed = parser.Parse(file);
+    EXPECT_FALSE(parsed);
+    EXPECT_EQ(parsed.errors.size(), 1);
+    EXPECT_EQ(parsed.errors[0].first, ConfigParseStatus::RequiredKeyNotFound);
+    for (auto& error : parsed.errors)
+    {
+      std::cout << error.second << " " << configParseStatusToString(error.first) << std::endl;
+    }
   }
 }
 
 TEST(TroeConfig, ParseConfig)
 {
-  micm::SolverConfig solver_config;
+  v0::Parser parser;
+  std::vector<std::string> extensions = { ".json", ".yaml" };
 
-  EXPECT_NO_THROW(solver_config.ReadAndParse("./v0_unit_configs/troe/valid"));
-
-  micm::SolverParameters solver_params = solver_config.GetSolverParams();
-
-  auto& process_vector = solver_params.processes_;
-
-  // Convert Arrhenius parameters from expecting molecules cm-3 to moles m-3
-  const double conv = 1.0e-6 * 6.02214076e23;
-
-  // first reaction
+  for (auto& extension : extensions)
   {
-    EXPECT_EQ(process_vector[0].reactants_.size(), 3);
-    EXPECT_EQ(process_vector[0].reactants_[0].name_, "foo");
-    EXPECT_EQ(process_vector[0].reactants_[1].name_, "quz");
-    EXPECT_EQ(process_vector[0].reactants_[2].name_, "quz");
-    EXPECT_EQ(process_vector[0].products_.size(), 2);
-    EXPECT_EQ(process_vector[0].products_[0].first.name_, "bar");
-    EXPECT_EQ(process_vector[0].products_[0].second, 1.0);
-    EXPECT_EQ(process_vector[0].products_[1].first.name_, "baz");
-    EXPECT_EQ(process_vector[0].products_[1].second, 3.2);
-    micm::TroeRateConstant* ternary_rate_constant =
-        dynamic_cast<micm::TroeRateConstant*>(process_vector[0].rate_constant_.get());
-    auto& params = ternary_rate_constant->parameters_;
-    // NVHPC compiler has some round-off error
-    EXPECT_NEAR(params.k0_A_, 1.0 * std::pow(conv, 3), std::pow(conv, 3) * 1e-12);
-    EXPECT_EQ(params.k0_B_, 0.0);
-    EXPECT_EQ(params.k0_C_, 0.0);
-    EXPECT_EQ(params.kinf_A_, 1.0 * std::pow(conv, 2));
-    EXPECT_EQ(params.kinf_B_, 0.0);
-    EXPECT_EQ(params.kinf_C_, 0.0);
-    EXPECT_EQ(params.Fc_, 0.6);
-    EXPECT_EQ(params.N_, 1.0);
-  }
+    std::string file = "./v0_unit_configs/troe/valid/config" + extension;
+    auto parsed = parser.Parse(file);
+    EXPECT_TRUE(parsed);
+    v0::types::Mechanism mechanism = *parsed;
 
-  // second reaction
-  {
-    EXPECT_EQ(process_vector[1].reactants_.size(), 2);
-    EXPECT_EQ(process_vector[1].reactants_[0].name_, "bar");
-    EXPECT_EQ(process_vector[1].reactants_[1].name_, "baz");
-    EXPECT_EQ(process_vector[1].products_.size(), 2);
-    EXPECT_EQ(process_vector[1].products_[0].first.name_, "bar");
-    EXPECT_EQ(process_vector[1].products_[0].second, 0.5);
-    EXPECT_EQ(process_vector[1].products_[1].first.name_, "foo");
-    EXPECT_EQ(process_vector[1].products_[1].second, 1.0);
-    micm::TroeRateConstant* ternary_rate_constant =
-        dynamic_cast<micm::TroeRateConstant*>(process_vector[1].rate_constant_.get());
-    auto& params = ternary_rate_constant->parameters_;
-    EXPECT_EQ(params.k0_A_, 32.1 * std::pow(conv, 2));
-    EXPECT_EQ(params.k0_B_, -2.3);
-    EXPECT_EQ(params.k0_C_, 102.3);
-    EXPECT_EQ(params.kinf_A_, 63.4 * conv);
-    EXPECT_EQ(params.kinf_B_, -1.3);
-    EXPECT_EQ(params.kinf_C_, 908.5);
-    EXPECT_EQ(params.Fc_, 1.3);
-    EXPECT_EQ(params.N_, 32.1);
+    auto& process_vector = mechanism.reactions.troe;
+    EXPECT_EQ(process_vector.size(), 2);
+
+    // first reaction
+    {
+      EXPECT_EQ(process_vector[0].reactants.size(), 2);
+      EXPECT_EQ(process_vector[0].reactants[0].species_name, "foo");
+      EXPECT_EQ(process_vector[0].reactants[0].coefficient, 1.0);
+      EXPECT_EQ(process_vector[0].reactants[1].species_name, "quz");
+      EXPECT_EQ(process_vector[0].reactants[1].coefficient, 2.0);
+      EXPECT_EQ(process_vector[0].products.size(), 2);
+      EXPECT_EQ(process_vector[0].products[0].species_name, "bar");
+      EXPECT_EQ(process_vector[0].products[0].coefficient, 1.0);
+      EXPECT_EQ(process_vector[0].products[1].species_name, "baz");
+      EXPECT_EQ(process_vector[0].products[1].coefficient, 3.2);
+      EXPECT_EQ(process_vector[0].k0_A, 1.0 * std::pow(conversions::MolesM3ToMoleculesCm3, 3));
+      EXPECT_EQ(process_vector[0].k0_B, 0.0);
+      EXPECT_EQ(process_vector[0].k0_C, 0.0);
+      EXPECT_EQ(process_vector[0].kinf_A, 1.0 * std::pow(conversions::MolesM3ToMoleculesCm3, 2));
+      EXPECT_EQ(process_vector[0].kinf_B, 0.0);
+      EXPECT_EQ(process_vector[0].kinf_C, 0.0);
+      EXPECT_EQ(process_vector[0].Fc, 0.6);
+      EXPECT_EQ(process_vector[0].N, 1.0);
+    }
+
+    // second reaction
+    {
+      EXPECT_EQ(process_vector[1].reactants.size(), 2);
+      EXPECT_EQ(process_vector[1].reactants[0].species_name, "bar");
+      EXPECT_EQ(process_vector[1].reactants[1].species_name, "baz");
+      EXPECT_EQ(process_vector[1].products.size(), 2);
+      EXPECT_EQ(process_vector[1].products[0].species_name, "bar");
+      EXPECT_EQ(process_vector[1].products[0].coefficient, 0.5);
+      EXPECT_EQ(process_vector[1].products[1].species_name, "foo");
+      EXPECT_EQ(process_vector[1].products[1].coefficient, 1.0);
+      EXPECT_EQ(process_vector[1].k0_A, 32.1 * std::pow(conversions::MolesM3ToMoleculesCm3, 2));
+      EXPECT_EQ(process_vector[1].k0_B, -2.3);
+      EXPECT_EQ(process_vector[1].k0_C, 102.3);
+      EXPECT_EQ(process_vector[1].kinf_A, 63.4 * conversions::MolesM3ToMoleculesCm3);
+      EXPECT_EQ(process_vector[1].kinf_B, -1.3);
+      EXPECT_EQ(process_vector[1].kinf_C, 908.5);
+      EXPECT_EQ(process_vector[1].Fc, 1.3);
+      EXPECT_EQ(process_vector[1].N, 32.1);
+    }
   }
 }
 
 TEST(TroeConfig, DetectsNonstandardKeys)
 {
-  micm::SolverConfig solver_config;
-
-  try
+  v0::Parser parser;
+  std::vector<std::string> extensions = { ".json", ".yaml" };
+  for (auto& extension : extensions)
   {
-    solver_config.ReadAndParse("./v0_unit_configs/troe/contains_nonstandard_key");
-  }
-  catch (const std::system_error& e)
-  {
-    EXPECT_EQ(e.code().value(), static_cast<int>(MicmConfigErrc::ContainsNonStandardKey));
+    std::string file = "./v0_unit_configs/troe/contains_nonstandard_key/config" + extension;
+    auto parsed = parser.Parse(file);
+    EXPECT_FALSE(parsed);
+    EXPECT_EQ(parsed.errors.size(), 8);
+    EXPECT_EQ(parsed.errors[0].first, ConfigParseStatus::InvalidKey);
+    EXPECT_EQ(parsed.errors[1].first, ConfigParseStatus::InvalidKey);
+    EXPECT_EQ(parsed.errors[2].first, ConfigParseStatus::InvalidKey);
+    EXPECT_EQ(parsed.errors[3].first, ConfigParseStatus::InvalidKey);
+    EXPECT_EQ(parsed.errors[4].first, ConfigParseStatus::InvalidKey);
+    EXPECT_EQ(parsed.errors[5].first, ConfigParseStatus::InvalidKey);
+    EXPECT_EQ(parsed.errors[6].first, ConfigParseStatus::InvalidKey);
+    EXPECT_EQ(parsed.errors[7].first, ConfigParseStatus::InvalidKey);
+    for (auto& error : parsed.errors)
+    {
+      std::cout << error.second << " " << configParseStatusToString(error.first) << std::endl;
+    }
   }
 }
 
 TEST(TroeConfig, DetectsNonstandardProductCoefficient)
 {
-  micm::SolverConfig solver_config;
-
-  try
+  v0::Parser parser;
+  std::vector<std::string> extensions = { ".json", ".yaml" };
+  for (auto& extension : extensions)
   {
-    solver_config.ReadAndParse("./v0_unit_configs/troe/nonstandard_product_coef");
-  }
-  catch (const std::system_error& e)
-  {
-    EXPECT_EQ(e.code().value(), static_cast<int>(MicmConfigErrc::ContainsNonStandardKey));
+    std::string file = "./v0_unit_configs/troe/nonstandard_product_coef/config" + extension;
+    auto parsed = parser.Parse(file);
+    EXPECT_FALSE(parsed);
+    EXPECT_EQ(parsed.errors.size(), 1);
+    EXPECT_EQ(parsed.errors[0].first, ConfigParseStatus::InvalidKey);
+    for (auto& error : parsed.errors)
+    {
+      std::cout << error.second << " " << configParseStatusToString(error.first) << std::endl;
+    }
   }
 }
 
 TEST(TroeConfig, DetectsNonstandardReactantCoefficient)
 {
-  micm::SolverConfig solver_config;
-
-  try
+  v0::Parser parser;
+  std::vector<std::string> extensions = { ".json", ".yaml" };
+  for (auto& extension : extensions)
   {
-    solver_config.ReadAndParse("./v0_unit_configs/troe/nonstandard_reactant_coef");
-  }
-  catch (const std::system_error& e)
-  {
-    EXPECT_EQ(e.code().value(), static_cast<int>(MicmConfigErrc::ContainsNonStandardKey));
+    std::string file = "./v0_unit_configs/troe/nonstandard_reactant_coef/config" + extension;
+    auto parsed = parser.Parse(file);
+    EXPECT_FALSE(parsed);
+    EXPECT_EQ(parsed.errors.size(), 1);
+    EXPECT_EQ(parsed.errors[0].first, ConfigParseStatus::InvalidKey);
+    for (auto& error : parsed.errors)
+    {
+      std::cout << error.second << " " << configParseStatusToString(error.first) << std::endl;
+    }
   }
 }
