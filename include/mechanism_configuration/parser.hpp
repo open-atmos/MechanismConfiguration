26c0522ffc99cf044a550c22d297bf9d604cac1d
Kyle Shores <kyle.shores44@gmail.com>
2025-02-20 11:44:12 -0600

    maybe now

--- include/mechanism_configuration/parser.hpp
+++ include/mechanism_configuration/parser.hpp
@@ -27,18 +27,36 @@ namespace mechanism_configuration
       std::cout << "File exists: " << config_path << std::endl;
 
       v1::Parser v1_parser;
-        return result;
+      std::cout << "Attempting to parse with v1 parser" << std::endl;
+      auto v1_result = v1_parser.Parse(config_path);
+
+      if (v1_result)
+      {
+      std::cout << "v1 parser succeeded" << std::endl;
+      result.mechanism = std::move(v1_result.mechanism);
+      return result;
+      }
+      else
+      {
+      std::cout << "v1 parser failed" << std::endl;
       }
 
       v0::Parser v0_parser;
+      std::cout << "Attempting to parse with v0 parser" << std::endl;
       auto v0_result = v0_parser.Parse(config_path);
 
       if (v0_result)
       {
-        result.mechanism = std::move(v0_result.mechanism);
-        return result;
+      std::cout << "v0 parser succeeded" << std::endl;
+      result.mechanism = std::move(v0_result.mechanism);
+      return result;
+      }
+      else
+      {
+      std::cout << "v0 parser failed" << std::endl;
       }
 
+      std::cout << "Both parsers failed" << std::endl;
       result.errors = v1_result.errors;
       result.errors.insert(result.errors.end(), v0_result.errors.begin(), v0_result.errors.end());
       return result;
