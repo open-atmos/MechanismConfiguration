67a7032f4654f36186625eac0c986661511d6d2d
Kyle Shores <kyle.shores44@gmail.com>
2025-02-20 09:19:42 -0600

    updating name

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -22,4 +22,4 @@ FetchContent_Declare(yaml-cpp
     GIT_REPOSITORY https://github.com/jbeder/yaml-cpp.git
     GIT_TAG 0.8.0
 )
-FetchContent_MakeAvailable(yaml)
+FetchContent_MakeAvailable(yaml-cpp)
