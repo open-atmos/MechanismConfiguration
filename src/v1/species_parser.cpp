6316b9614f5ea20b87e39e37af39213a63addf0e
Kyle Shores <kyle.shores44@gmail.com>
2025-03-06 10:00:58 -0600

    fixing merge request I guess

--- src/v1/species_parser.cpp
+++ src/v1/species_parser.cpp
@@ -15,14 +15,22 @@ namespace mechanism_configuration
       for (const auto& object : objects)
       {
         types::Species species;
-        auto validate = ValidateSchema(object, validation::species.required_keys, validation::species.optional_keys);
+        auto required_keys = { validation::keys.name };
+        auto optional_keys = { validation::keys.absolute_tolerance,
+                                                      validation::keys.diffusion_coefficient,
+                                                      validation::keys.molecular_weight,
+                                                      validation::keys.henrys_law_constant_298,
+                                                      validation::keys.henrys_law_constant_exponential_factor,
+                                                      validation::keys.n_star,
+                                                      validation::keys.density };
+        auto validate = ValidateSchema(object, required_keys, optional_keys);
         errors.insert(errors.end(), validate.begin(), validate.end());
         if (validate.empty())
         {
           std::string name = object[validation::keys.name].as<std::string>();
 
           std::map<std::string, double> numerical_properties{};
-          for (const auto& key : validation::species.optional_keys)
+          for (const auto& key : optional_keys)
           {
             if (object[key])
             {
@@ -33,7 +41,7 @@ namespace mechanism_configuration
 
           species.name = name;
           species.optional_numerical_properties = numerical_properties;
-          species.unknown_properties = GetComments(object, validation::species.required_keys, validation::species.optional_keys);
+          species.unknown_properties = GetComments(object, required_keys, optional_keys);
 
           all_species.push_back(species);
         }
