23bfa36a541d0077ce68989c5c88457fb99e1053
Kyle Shores <kyle.shores44@gmail.com>
2025-03-12 14:15:08 -0500

    disabling build of yaml tools

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -33,6 +33,9 @@ FetchContent_Declare(yaml-cpp
     GIT_REPOSITORY https://github.com/jbeder/yaml-cpp.git
     GIT_TAG 0.8.0
 )
+
+set(YAML_CPP_BUILD_TOOLS OFF CACHE BOOL "" FORCE)
+
 FetchContent_MakeAvailable(yaml-cpp)
 
 ################################################################################
